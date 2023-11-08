9c9d5f6dbdba23e93e56f3454f5cea49c240c68b
Asura0107 <mircoorosio@gmail.com>
2023-11-08 16:21:33 +0100

    U2-W3-D3-fetch

--- script.js
+++ script.js
@@ -12,20 +12,35 @@ fetch("https://striveschool-api.herokuapp.com/books")
     for (let i = 0; i < userObj.length; i++) {
       const row = document.querySelector(".row");
       const item = userObj[i];
+
       const col = document.createElement("div");
-      col.className = "card col-3";
+      col.className = "card col-3 gy-3 ";
 
       const img = document.createElement("img");
-      img.className = "w-100 object-fit-cover ";
+      img.className = "card-img-top object-fit-cover";
       img.style.height = "200px";
       img.src = item.img;
 
       const cardbody = document.createElement("div");
-      const title = document.createElement("h5");
+      const title = document.createElement("h6");
       title.innerText = item.title;
-      col.appendChild(img);
+      title.className = "card-title";
+
+      const btnwaste = document.createElement("button");
+      btnwaste.innerText = "Delate";
+      btnwaste.onclick = () => {
+        col.className = "d-none";
+      };
+
+      const price = document.createElement("p");
+      price.innerText = "$ " + item.price;
+      price.className = "card-text";
       row.appendChild(col);
+      col.appendChild(img);
       cardbody.appendChild(title);
+      col.appendChild(cardbody);
+      cardbody.appendChild(price);
+      cardbody.appendChild(btnwaste);
     }
 
     // userObj.data.forEach((card) => {
